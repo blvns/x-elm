7a02deda0507f74666c08e28312dbb931464592e
Terra <blvns@cs.washington.edu>
2023-06-23 14:36:34 -0700

    Update eval_cbtm.sh

--- xl-btm/eval_cbtm.sh
+++ xl-btm/eval_cbtm.sh
@@ -8,18 +8,18 @@ mkdir -p ${EVAL_DIR};
 
 # get model checkpoints
 CONSOLIDATED_MODEL_PATHS=$3;
-CONSOLIDATED_MODEL_PATHS=`echo "${CONSOLIDATED_MODEL_PATHS}" | tr ',' ' '`
+#CONSOLIDATED_MODEL_PATHS=`echo "${CONSOLIDATED_MODEL_PATHS}" | tr ',' ' '`
 echo $CONSOLIDATED_MODEL_PATHS
 
 # these model paths should be ordered by cluster ID!
-JOINED_MODEL_PATHS=$(join ${CONSOLIDATED_MODEL_PATHS[@]})
-echo $JOINED_MODEL_PATHS
+#JOINED_MODEL_PATHS=$(join ${CONSOLIDATED_MODEL_PATHS[@]})
+#echo $JOINED_MODEL_PATHS
 
 #python -m metaseq_cli.eval_cbtm \
 #    --data-dir ${DATA_DIR}/${DATASET} \
 #    --data-subset valid/C4_small \
 #    --path-to-clusterer ${KMEANS_DIR}/${DATASET}/${NUM_CLUSTERS}/ \
-#    --model-paths $(join ${CONSOLIDATED_MODEL_PATHS[@]}) \
+#    --model-paths $CONSOLIDATED_MODEL_PATHS \
 #    --job-dir ${EVAL_DIR} \
 #    --temperature 0.1 \
 #    --max-valid-steps 200 \
