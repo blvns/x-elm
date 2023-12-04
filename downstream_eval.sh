13e889550ecebe7582c802e7c5c1b17c9d5926e7
tomasz <t.limisiewicz@gmail.com>
2023-12-04 15:36:52 -0800

    Update evaluation script

--- downstream_eval.sh
+++ downstream_eval.sh
@@ -1,15 +1,39 @@
 
+cd /gscratch/zlab/tomlim/mbtm/xl-btm || exit
 
-BASE_DIR=<your_directory>
+. "/gscratch/zlab/tomlim/miniconda3/etc/profile.d/conda.sh"
+export PATH="/gscratch/zlab/tomlim/my_gs/miniconda3/bin:$PATH"
+
+conda activate cbtm
+
+# SET UP VARIABLES
+echo "Setting up variables"
+
+BASE_DIR="/gscratch/zlab/tomlim/mbtm"
+
 #baseline
-#MODEL="facebook/xglm-1.7B"
-# 10.4B model
-#MODEL="${BASE_DIR}/experiments/1_clusters/xlbtm.dense.mu20000.cluster0/hf"
-# 20.9B model
-MODEL="${BASE_DIR}/experiments/1_clusters/xlbtm.dense.mu40000.cluster0/hf"
-TASK="xnli"
-LANGUAGES=('ar' 'bg' 'de' 'el' 'en' 'es' 'fr' 'hi' 'ru' 'sw' 'th' 'tr' 'ur' 'vi' 'zh')
-RESULTS="${BASE_DIR}/results/20.9B"
+if [ "$MODEL_NAME" == "xglm-1.7B" ]; then
+    MODEL="facebook/xglm-1.7B"
+elif [ "$MODEL_NAME" == "10.4B" ]; then
+    MODEL="${BASE_DIR}/experiments/1_clusters/xlbtm.dense.mu20000.cluster0/hf"
+elif [ "$MODEL_NAME" == "20.9B" ]; then
+    MODEL="${BASE_DIR}/experiments/1_clusters/xlbtm.dense.mu40000.cluster0/hf"
+else
+    echo "Model name not recognized"
+    exit 1
+fi
+
+if [ "$TASK" == "xnli" ]; then
+    LANGUAGES=('ar' 'bg' 'de' 'el' 'en' 'es' 'fr' 'hi' 'ru' 'sw' 'th' 'tr' 'ur' 'vi' 'zh')
+elif [ "$TASK" == "xstorycloze" ]; then
+    LANGUAGES=('ar' 'en' 'es' 'eu' 'hi' 'id' 'my' 'ru' 'sw' 'te' 'zh')
+else
+    echo "Task not recognized"
+    exit 1
+fi
+
+
+RESULTS="${BASE_DIR}/results/${MODEL_NAME}/dense"
 
 
 # RUN EVALUATION
