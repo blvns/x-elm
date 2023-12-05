5fd872c45dcc1afeca920f06e6c52e252a7de159
tomasz <t.limisiewicz@gmail.com>
2023-12-04 21:01:09 -0800

    Update evaluation script

--- job_clustered_downstream_eval.sh
+++ job_clustered_downstream_eval.sh
@@ -5,8 +5,9 @@
 TASK=$1
 MODEL_NAME=$2
 METHOD=$3
+CROSS=$4
 CLUSTER=$SLURM_ARRAY_TASK_ID
-nc = 8 # number of clusters (if changing need to change LANGUAGE_MAP)
+nc=8 # number of clusters (if changing need to change LANGUAGE_MAP)
 
 shopt -s nullglob
 
@@ -21,38 +22,53 @@ export PATH="/gscratch/zlab/tomlim/my_gs/miniconda3/bin:$PATH"
 conda activate cbtm
 
 if [ "$TASK" == "xnli" ]; then
-  LANGUAGE_MAP = ('en es' 'de el' '' 'hi ur' 'bg ru' 'fr ar' 'tr zh' 'vi zh')
+  LANGUAGE_MAP=('en es' 'de el' '' 'hi ur' 'bg ru' 'fr ar' 'tr zh' 'vi zh')
 elif [ "$TASK" == "xstorycloze" ]; then
-  LANGUAGE_MAP = ('en es' '' '' 'hi' 'ru' 'ar' 'zh' 'sw')
+  LANGUAGE_MAP=('en es' '' '' 'hi' 'ru' 'ar' 'zh' 'sw')
 else
   echo "Task not recognized"
   exit 1
 fi
 
-LANGUAGES = LANGUAGE_MAP[$CLUSTER]
-
-# check if there are languages to evaluate
-if [ "$LANGUAGES" == '' ]; then
-    echo "No languages to evaluate in cluster ${CLUSTER}"
-    exit 0
+LANGUAGES=${LANGUAGE_MAP[$CLUSTER]}
+# CROSS not equal to empty string means cross-lingual evaluation
+if [ "$CROSS" != '' ]; then
+    LANGUAGES=${LANGUAGE_MAP[$CLUSTER]}' '${LANGUAGE_MAP[$CROSS]}
 fi
 
 
 if [ "$MODEL_NAME" == "10.4B" ]; then
-    MODEL="${BASE_DIR}/experiments/${nc}_clusters/xlbtm.${METHOD}.mu20000.cluster${CLUSTER}/hf"
+    MODEL_PREFIX="${BASE_DIR}/experiments/${nc}_clusters/xlbtm.${METHOD}.mu20000.cluster"
 elif [ "$MODEL_NAME" == "20.9B" ] && [ "$METHOD" == "lang" ]; then
-    MODEL="${BASE_DIR}/experiments/${nc}_clusters_tl/xlbtm.${METHOD}.mu40000.cluster${CLUSTER}/hf"
+    MODEL_PREFIX="${BASE_DIR}/experiments/${nc}_clusters_tl/xlbtm.${METHOD}.mu40000.cluster"
 elif [ "$MODEL_NAME" == "20.9B" ]; then
-    MODEL="${BASE_DIR}/experiments/${nc}_clusters/xlbtm.${METHOD}.mu40000.cluster${CLUSTER}/hf"
+    MODEL_PREFIX="${BASE_DIR}/experiments/${nc}_clusters/xlbtm.${METHOD}.mu40000.cluster"
 else
     echo "Model name not recognized"
     exit 1
 fi
 
-RESULTS="${BASE_DIR}/results/${MODEL_NAME}/C${nc}_${METHOD}"
+if [ "$CROSS" == "" ] || [ "$CROSS" == "SRC" ] ; then
+    MODEL="${MODEL_PREFIX}${CLUSTER}/hf"
+elif [ "$METHOD" == "TRG" ]; then
+    MODEL="${MODEL_PREFIX}0/hf"
+elif [ "$CROSS" == "ENSEMBLE" ]; then
+    MODEL="${MODEL_PREFIX}0/hf ${MODEL_PREFIX}${CLUSTER}/hf"
+else
+  echo "Crosslingual mode not recognized"
+  exit 1
+fi
 
-# RUN EVALUATION
-echo "Running evaluation"
-python3 xl-btm/downstream_eval/prompt.py --model_path ${MODEL} --output_dir ${RESULTS} --task ${TASK} --eval_lang ${LANGUAGES}
 
+if [ "$CROSS" == "" ]; then
+  RESULTS="${BASE_DIR}/results/${MODEL_NAME}/C${nc}_${METHOD}"
+  # RUN EVALUATION
+  echo "Running evaluation for cluster ${CLUSTER} languages: ${LANGUAGES}"
+  python3 xl-btm/downstream_eval/prompt.py --model_path ${MODEL} --output_dir ${RESULTS} --task ${TASK} --eval_lang ${LANGUAGES}
+else
+  RESULTS="${BASE_DIR}/results/${MODEL_NAME}/C${nc}_${METHOD}_from_en_${CROSS}"
+  # RUN EVALUATION
+  echo "Running cross-lingual evaluation for cluster ${CLUSTER} languages: ${LANGUAGES}, from English examples with ${CROSS} model"
+  python3 xl-btm/downstream_eval/prompt.py --model_path ${MODEL} --output_dir ${RESULTS} --task ${TASK} --eval_lang ${LANGUAGES} --demo_lang "en" --k 8
+fi
 echo "Evaluation finished"
--- main_clustered_downstream_eval.sh
+++ main_clustered_downstream_eval.sh
@@ -7,7 +7,10 @@ shopt -s nullglob
 TASK=$1
 MODEL_NAME=$2
 METHOD=$3
+CROSS=${4:-""} # TRG or SRC or ENSEMBLE or empty string. Now assume that demonstration in English.
 nc=8 # number of clusters (if changing need to change LANGUAGE_MAP)
+# parameter with default value
 
 
-sbatch --array=0-${nc} job_clustered_downstream_eval.sh $TASK $MODEL_NAME $METHOD
+
+sbatch --array=0-$((nc-1)) job_clustered_downstream_eval.sh $TASK $MODEL_NAME $METHOD $CROSS
