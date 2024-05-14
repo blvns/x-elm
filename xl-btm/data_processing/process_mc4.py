86782cd8102ef17c07b144b8562b2cc744f457c3
Terra <blvns@cs.washington.edu>
2024-05-13 23:21:59 -0700

    Update process_mc4.py

--- xl-btm/data_processing/process_mc4.py
+++ xl-btm/data_processing/process_mc4.py
@@ -131,8 +131,8 @@ def main(args):
         for split in ['train', 'validation']: #not doing train
             #load to get full size of dataset
             if split == 'train' and lang == 'en': 
-		dataset_files = data_files = "multilingual/c4-en.tfrecord-0000*-of-*.json.gz"
-		mc4 = load_dataset('mc4', lang, split=split, dataset_files=dataset_files, cache_dir=PATH_TO_CACHE) #allenai/c4
+		    dataset_files = "multilingual/c4-en.tfrecord-0000*-of-*.json.gz"
+		    mc4 = load_dataset('mc4', lang, split=split, dataset_files=dataset_files, cache_dir=PATH_TO_CACHE) #allenai/c4
 	    else: mc4 = load_dataset('mc4', lang, split=split, cache_dir=PATH_TO_CACHE)
 
             #calculate which records go in which shards
